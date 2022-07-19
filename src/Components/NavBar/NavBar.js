67794d2de32978448bdefc9f8b8c5cd5894d0a0c
Ömer KAZ <trmn500@gmail.com>
2022-07-19 10:06:09 +0300

    When click a navbar link, added active classname!

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,26 +1,24 @@
-import {useRef} from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useRef } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import UserFilter from "../UserFilter/UserFilter";
 import UniversityFilter from "../UniversityFilter/UniversityFilter";
 import Home from "../Home/Home";
 import UserCreateForm from "../UserCreateForm/UserCreateForm";
-import "./NavBar.css"
-import NavBarİcon from "../../assets/icon.svg"
+import "./NavBar.css";
+import NavBarİcon from "../../assets/icon.svg";
 
 function NavBar() {
-
-  
   return (
     <Router>
       <nav className="navbar navbar-expand-lg">
         <div className="container">
           <a className="navbar-brand fw-bold" href="#">
-            <img
-              src={NavBarİcon}
-              width="30"
-              height="30"
-              alt=""
-            />
+            <img src={NavBarİcon} width="30" height="30" alt="" />
           </a>
           <button
             className="navbar-toggler"
@@ -38,24 +36,49 @@ function NavBar() {
             id="navbarNavAltMarkup"
           >
             <div className="navbar-nav ">
-              <Link className="nav-link btn fw-semibold mx-1" to="/">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "active nav-link btn fw-semibold mx-1"
+                    : "nav-link btn fw-semibold mx-1"
+                }
+                to="/"
+              >
                 Home
-              </Link>
+              </NavLink>
 
-              <Link
-                className="nav-link btn fw-semibold mx-1"
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "active nav-link btn fw-semibold mx-1"
+                    : "nav-link btn fw-semibold mx-1"
+                }
                 to="/universityFilter"
               >
                 University Search
-              </Link>
+              </NavLink>
 
-              <Link className="nav-link btn fw-semibold mx-1" to="/userFilter">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "active nav-link btn fw-semibold mx-1"
+                    : "nav-link btn fw-semibold mx-1"
+                }
+                to="/userFilter"
+              >
                 User Search
-              </Link>
+              </NavLink>
 
-              <Link className="nav-link btn fw-semibold mx-1" to="/createUser">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive
+                    ? "active nav-link btn fw-semibold mx-1"
+                    : "nav-link btn fw-semibold mx-1"
+                }
+                to="/createUser"
+              >
                 Create User
-              </Link>
+              </NavLink>
             </div>
           </div>
         </div>
